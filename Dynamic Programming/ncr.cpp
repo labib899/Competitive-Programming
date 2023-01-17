77ea4fe580c5901e4ea4d18074cef68b8c0b0044
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-01-18 04:43:56 +0600

    Update ncr.cpp

--- Dynamic Programming/ncr.cpp
+++ Dynamic Programming/ncr.cpp
@@ -1,26 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> vi;
-typedef long long ll;
+typedef long long  ll;
+typedef vector<ll> vll;
 
 const ll N=1e3+10;
-const ll M=1e3+10;
 ll mod=1e9+7;
-vector<vi> dp(N+1,vi(M+1,-1));
-ll ncr(int n,int r);
+vector<vll> dp(N,vll(N,-1));
 
 
-int main()
-{
-    int n,r; cin>>n>>r;
-    cout<<ncr(n,r)<<endl;
-}
-
-ll ncr(int n,int r)
+ll ncr(ll n,ll r)
 {
     if(r==0 || r==n) return 1;
     if(r==1) return n;
     if(dp[n][r]!=-1) return dp[n][r]%mod;
     return dp[n][r]=(ncr(n-1,r-1)+ncr(n-1,r))%mod;
+}
+
+
+int main()
+{
+    ll n,r; cin>>n>>r;
+    cout<<ncr(n,r)<<endl;
 }
