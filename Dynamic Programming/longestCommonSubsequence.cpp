d6897740235a8985e0563ebcdae0ef7bbab93c64
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-01-18 03:51:49 +0600

    Update longestCommonSubsequence.cpp

--- Dynamic Programming/longestCommonSubsequence.cpp
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -1,31 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef vector<int> vi;
+
 const int N=1e3+10;
 const int M=1e3+10;
-int dp[N][M];
-int lcs(int i,int j,string s,string t)
+vector<vi> dp(N,vi(M,-1));
+string s,t;
+
+
+int lcs(int i,int j)
 {
-    for(int j=0;j<M;j++) dp[0][j]=0;
-    for(int i=0;i<N;i++) dp[i][0]=0;
-    for(int i=1;i<N;i++)
-    {
-        for(int j=1;j<M;j++)
-        {
-            if(s[i]==t[j]) dp[i][j]=dp[i-1][j-1]+1;
-            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        }
-    }
+    if(i<0 || j<0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==t[j]) dp[i][j]=lcs(i-1,j-1)+1;
+    else dp[i][j]=max(lcs(i-1,j),lcs(i,j-1));
     return dp[i][j];
 }
 
-string printLCS(string s,string t)
+string printLCS()
 {
-    int len=dp[s.size()-1][t.size()-1];
+    int n=s.size(),m=t.size();
+    int len=dp[n-1][m-1];
     string ans;
-    int idx=len-1;
     for(int i=0;i<len;i++) ans+="$";
-    int i=s.size()-1,j=t.size()-1;
+    int i=n-1,j=m-1;
+    int idx=len-1;
     while(i>=0 && j>=0)
     {
         if(s[i]==t[j]) ans[idx--]=s[i],i--,j--;
@@ -38,7 +38,10 @@ string printLCS(string s,string t)
 
 int main()
 {
-    string s,t; cin>>s>>t;
+    cin>>s>>t;
     int n=s.size(),m=t.size();
-    cout<<lcs(n-1,m-1,s,t)<<endl<<printLCS(s,t)<<endl;
+    cout<<lcs(n-1,m-1)<<endl;
+    cout<<printLCS()<<endl;
 }
+
+
