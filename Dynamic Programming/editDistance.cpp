b69005beefd50342678eb394956e2dc490b9031f
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-01-18 04:05:41 +0600

    Update editDistance.cpp

--- Dynamic Programming/editDistance.cpp
+++ Dynamic Programming/editDistance.cpp
@@ -1,27 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define min3(a,b,c) min(a,min(b,c))
 typedef vector<int> vi;
 
 const int N=1e3+10;
 const int M=1e3+10;
 vector<vi> dp(N,vi(M,-1));
-int ed(int i,int j,string s,string t);
+string s,t;
 
 
-int main()
-{
-    string s,t; cin>>s>>t;
-    int n=s.size(),m=t.size();
-    cout<<ed(n-1,m-1,s,t)<<endl;
-}
-
-int ed(int i,int j,string s,string t)
+int ed(int i,int j)
 {
     if(i<0) return j+1;
     if(j<0) return i+1;
     if(dp[i][j]!=-1) return dp[i][j];
-    if(s[i]==t[j]) dp[i][j]=ed(i-1,j-1,s,t);
-    else dp[i][j]=min(ed(i-1,j-1,s,t),min(ed(i-1,j,s,t),ed(i,j-1,s,t)))+1;
+    if(s[i]==t[j]) dp[i][j]=ed(i-1,j-1);
+    else dp[i][j]=1+min3(ed(i-1,j-1),ed(i-1,j),ed(i,j-1));
     return dp[i][j];
 }
+
+
+int main()
+{
+    cin>>s>>t;
+    int n=s.size(),m=t.size();
+    cout<<ed(n-1,m-1)<<endl;
+}
+
