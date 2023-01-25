81285824e529cb0ec0b0375cae11a9754d27160d
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-01-25 16:04:13 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 //using namespace __gnu_pbds;
  
-#define fastIO                  ios::sync_with_stdio(0);cin.tie(0);
+#define fastIO                  ios::sync_with_stdio(0); cin.tie(0);
 #define fRead(x)                freopen(x,"r",stdin)
 #define fWrite(x)               freopen(x,"w",stdout)
 #define endl                    "\n"
 #define pb                      push_back
 #define mp                      make_pair
 #define all(x)                  x.begin(),x.end()
-#define forn(i,l,r,x)           for(ll i=l;i<=r;i+=x)
+#define rep(i,l,r)              for(ll i=l;i<r;i++)
 #define min3(a,b,c)             min(a,min(b,c))
 #define min4(a,b,c,d)           min(a,min(b,min(c,d)))
 #define max3(a,b,c)             max(a,max(b,c))
 #define max4(a,b,c,d)           max(a,max(b,max(c,d)))
-#define print(v)                for(auto x:v) cout<<x<<" ";cout<<endl;
+#define print(v)                for(auto x:v) cout<<x<<" "; cout<<endl;
 #define F                       first
 #define S                       second
 typedef long long               ll;
@@ -37,10 +37,10 @@ void solve();
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        //fRead("input.txt");
-        //fWrite("output.txt");
-    #endif
+#ifndef ONLINE_JUDGE
+    //fRead("input.txt");
+    //fWrite("output.txt");
+#endif
     fastIO;
     ll t=1; cin>>t;
     while(t--) solve();
