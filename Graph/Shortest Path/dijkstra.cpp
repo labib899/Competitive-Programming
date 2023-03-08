8faba1979eaf9d994ffde656606bf2af892f3675
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2023-03-08 19:58:56 +0600

    Update dijkstra.cpp

--- Graph/Shortest Path/dijkstra.cpp
+++ Graph/Shortest Path/dijkstra.cpp
@@ -11,37 +11,37 @@ typedef pair<int,int>	pii;
 
 int main()
 {
-	int n,m; cin>>n>>m;
-	vector<pii> adj[n+2];
-	for(int i=1;i<=m;i++)
+    int n,m; cin>>n>>m;
+    vector<pii> adj[n+2];
+    for(int i=1;i<=m;i++)
+    {
+        int a,b,w; cin>>a>>b>>w;
+        adj[a].pb({b,w});
+        adj[b].pb({a,w});
+    }
+    vi distance(n+2,INT_MAX);
+    int source; cin>>source; 
+    distance[source]=0;
+    PQ<pii,vector<pii>,greater<pii>> pq;
+    pq.push({0,source}); // push {dis,node}
+    while(pq.size())
+    {	
+	int d=pq.top().ff;
+	int node=pq.top().ss; 
+	pq.pop();
+	for(auto x:adj[node])
 	{
-		int a,b,w; cin>>a>>b>>w;
-		adj[a].pb({b,w});
-		adj[b].pb({a,w});
+	    int adjNode=x.ff,w=x.ss;
+	    if(d+w<distance[adjNode])
+	    {
+		distance[adjNode]=d+w;
+		pq.push({distance[adjNode],adjNode});
+	    }
 	}
-	vi distance(n+2,INT_MAX);
-	int source; cin>>source; 
-	distance[source]=0;
-	PQ<pii,vector<pii>,greater<pii>> pq;
-	pq.push({0,source}); // push {dis,node}
-	while(pq.size())
-	{	
-		int d=pq.top().ff;
-		int node=pq.top().ss; 
-		pq.pop();
-		for(auto x:adj[node])
-		{
-			int adjNode=x.ff,w=x.ss;
-			if(d+w<distance[adjNode])
-			{
-				distance[adjNode]=d+w;
-				pq.push({distance[adjNode],adjNode});
-			}
-		}
-	}
-	// printing solution
-	for(int i=0;i<n;i++) cout<<distance[i]<<" ";
-	cout<<endl;
+    }
+    // printing solution
+    for(int i=0;i<n;i++) cout<<distance[i]<<" ";
+    cout<<endl;
 }
 
 
