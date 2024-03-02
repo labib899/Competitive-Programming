062ad926228a5cb51e87de865aa03b53b3257d49
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2024-03-03 04:21:47 +0600

    Add files via upload

---
+++ Template/debug.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<": ";_print(x);cerr<<endl;
+#else 
+#define debug(x)
+#endif
+
+void _print(int x) {cerr<<x;}
+void _print(long long x) {cerr<<x;}
+void _print(char x) {cerr<<x;}
+void _print(string x) {cerr<<x;}
+void _print(double x) {cerr<<x;}
+void _print(long double x) {cerr<<x;}
+
+template<class T> void _print(vector<T> v);
+template<class T> void _print(set<T> s);
+template<class T> void _print(multiset<T> ms);
+template<class T,class V> void _print(pair<T,V> p);
+template<class T,class V> void _print(map<T,V> m);
+template<class T> void _print(vector<T> v) {cerr<<"[ ";for(T x:v){_print(x);cerr<<" ";}cerr<<"]";}
+template<class T> void _print(set<T> s) {cerr<<"[ ";for(T x:s){_print(x);cerr<<" ";}cerr<<"]";}
+template<class T> void _print(multiset<T> ms) {cerr<<"[ ";for(T x:ms){_print(x);cerr<<" ";}cerr<<"]";}
+template<class T,class V> void _print(pair<T,V> p) {cerr<<"{";_print(p.ff);cerr<<",";_print(p.ss);cerr<<"}";}
+template<class T,class V> void _print(map<T,V> m) {cerr<<"[ ";for(auto x:m) {_print(x);cerr<<" ";}cerr<<"]";}
