775f7951b74e75ccf140e9212ac26974c75aed54
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2024-03-07 19:05:27 +0600

    Add files via upload

---
+++ Template/macros.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <bits/stdtr1c++.h>
+//#include <bits/extc++.h>
+using namespace std;
+//using namespace __gnu_pbds;
+//using namespace __gnu_cxx;
+ 
+#define fastIO                  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define endl                    "\n"
+#define pb                      push_back
+#define mp                      make_pair
+#define mt                      make_tuple
+#define all(x)                  x.begin(),x.end()
+#define allrev(x)               x.rbegin(),x.rend()
+#define rep(i,l,r)              for(ll i=l;i<r;i++)
+#define min3(a,b,c)             min(a,min(b,c))
+#define min4(a,b,c,d)           min(a,min(b,min(c,d)))
+#define max3(a,b,c)             max(a,max(b,c))
+#define max4(a,b,c,d)           max(a,max(b,max(c,d)))
+#define print(v)                for(auto x:v) cout<<x<<" ";cout<<endl;
+#define ff                      first
+#define ss                      second
+#define PQ                      priority_queue
+#define __lcm(a,b)              (a/__gcd(a,b))*b
+typedef long long               ll;
+typedef long double             ld;
+typedef vector<int>             vi;
+typedef vector<vi>              vvi;
+typedef vector<ll>              vll;
+typedef vector<vll>             vvll;
+typedef vector<string>          vs;
+typedef pair<int,int>           pii;
+typedef pair<ll,ll>             pll;
+//typedef tuple<int,int,int>      tiii;
+//typedef tuple<ll,ll,ll>         tlll;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+const ll mod=1e9+7;
+const ll inf=LLONG_MAX;
